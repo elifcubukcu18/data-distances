45427ddc25bb5d8b954008cce1da2706ecef4adf
Elif Çubukcu <elifcubukcu@Elif-MacBook-Air-4.local>
2026-02-13 16:00:56 +0300

    test(utils): manhattan distance

--- tests/test_manhattan.py
+++ tests/test_manhattan.py
@@ -1,12 +1,12 @@
-from utils.distances import manhattan
+from utils.distances import manhattan_distance
 
 def test_manhattan():
-    assert manhattan((0, 0), (0, 0)) == 0
-    assert manhattan((0, 0), (1, 1)) == 2
-    assert manhattan((0, 0), (1, 0)) == 1
-    assert manhattan((0, 0), (0, 1)) == 1
-    assert manhattan((0, 0), (-1, 0)) == 1
-    assert manhattan((0, 0), (0, -1)) == 1
-    assert manhattan((0, 0), (-1, -1)) == 2
-    assert manhattan((0, 0), (1, -1)) == 2
-    assert manhattan((0, 0), (-1, 1)) == 2
+    assert manhattan_distance((0, 0), (0, 0)) == 0
+    assert manhattan_distance((0, 0), (1, 1)) == 2
+    assert manhattan_distance((0, 0), (1, 0)) == 1
+    assert manhattan_distance((0, 0), (0, 1)) == 1
+    assert manhattan_distance((0, 0), (-1, 0)) == 1
+    assert manhattan_distance((0, 0), (0, -1)) == 1
+    assert manhattan_distance((0, 0), (-1, -1)) == 2
+    assert manhattan_distance((0, 0), (1, -1)) == 2
+    assert manhattan_distance((0, 0), (-1, 1)) == 2
