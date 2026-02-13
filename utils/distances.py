657012c88bb2e48792caed5f9b8b36d6bdbcfaf6
Elif Çubukcu <elifcubukcu@Elif-MacBook-Air-4.local>
2026-02-13 16:14:04 +0300

    feat(utils): manhattan distanc,  euclidean and minkowski

--- utils/distances.py
+++ utils/distances.py
@@ -9,3 +9,18 @@ def euclidean(a, b):
     d_y = b[1] - a[1]
 
     return (d_x ** 2 + d_y ** 2) ** 0.5
+
+def manhattan(a, b):
+    return minkowski(a, b, 1)
+
+
+def euclidean(a, b):
+    return minkowski(a, b, 2)
+
+
+def minkowski(a, b, p):
+    d_x = b[0] - a[0]
+    d_y = b[1] - a[1]
+
+    distance = (abs(d_x)**p + abs(d_y)**p)**(1/p)
+    return distance
