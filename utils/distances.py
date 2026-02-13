a526657d56c344af372f7cb078fde9ed27df8920
Elif Çubukcu <elifcubukcu@Elif-MacBook-Air-4.local>
2026-02-13 16:10:38 +0300

    feat(utils): manhattan distance and euclidean

--- utils/distances.py
+++ utils/distances.py
@@ -2,3 +2,10 @@ def manhattan_distance(point1, point2):
     d_x = abs(point2[0] - point1[0])
     d_y = abs(point2[1] - point1[1])
     return d_x + d_y
+
+def euclidean(a, b):
+
+    d_x = b[0] - a[0]
+    d_y = b[1] - a[1]
+
+    return (d_x ** 2 + d_y ** 2) ** 0.5
